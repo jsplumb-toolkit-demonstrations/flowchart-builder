8e1624ae54e2494152566e7061faaa77e30eab5a
Simon Porritt <simon@jsplumbtoolkit.com>
2021-10-26 16:08:16 +1100

    updated dialogs to new syntax

--- ts/demo.ts
+++ ts/demo.ts
@@ -58,23 +58,23 @@ ready(() => {
 // ------------------------- dialogs -------------------------------------
 
     const dialogs = Dialogs.newInstance({
-        dialogs: {
-            "dlgText":[
-                '<input type="text" size="50" jtk-focus jtk-att="text" value="${text}" jtk-commit="true"/>',
-                'Enter Text',
-                true
 
-            ],
-            "dlgConfirm":[
-                '${msg}',
-                'Please Confirm',
-                true
-            ],
-            "dlgMessage":[
-                '${msg}',
-                "Message",
-                false
-            ]
+        dialogs: {
+            "dlgText": {
+                template: '<input type="text" size="50" jtk-focus jtk-att="text" value="${text}" jtk-commit="true"/>',
+                title: 'Enter Text',
+                cancelable: true
+            },
+            "dlgConfirm":{
+                template:'${msg}',
+                title:'Please Confirm',
+                cancelable:true
+            },
+            "dlgMessage": {
+                template:'${msg}',
+                title:"Message",
+                cancelable:false
+            }
         }
     });
 
